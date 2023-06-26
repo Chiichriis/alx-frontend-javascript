1781b5e150e30ac1f2fdb1f0a5637679fe9e3ac3
chiichriis <chichris28@gmail.com>
2023-06-26 12:59:40 +0000

    advance tsk

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,11 +1,8 @@
-
-rt default function createIteratorObject(report) {
-  let allEmployees = [];
-  for (const item of Object.values(report.allEmployees)) {
-    allEmployees = [
-      ...allEmployees,
-      ...item,
-    ];
+export default function* createIteratorObject(report) {
+  for (const department in report) {
+    const employees = report[department];
+    for (const employee of employees) {
+      yield employee;
+    }
   }
-  return allEmployees;
 }
